16856010845d89b16349fa853c0a214727c2ec9d
D. Fleury <dfleury2@free.fr>
2013-07-01 22:14:27 +0200

    Adding unit tests for Ne_Text_Buffer

--- NEditUnitTests/test_Ne_Text_Buffer.cpp
+++ NEditUnitTests/test_Ne_Text_Buffer.cpp
@@ -286,3 +286,232 @@ TEST_F(Ne_Text_Buffer_Test, SubstituteNullChar)
    BufUnsubstituteNullChars(str, buffer);
    EXPECT_EQ(str[4], '\0');
 }
+
+// --------------------------------------------------------------------------
+TEST_F(Ne_Text_Buffer_Test, ExpandCharWidth)
+{
+   EXPECT_EQ(BufCharWidth('\0', 4, 3, '\0'), 5);
+   EXPECT_EQ(BufCharWidth('\1', 4, 3, '\0'), 5);
+
+   // Tabs and indent
+   // Example:
+   // tabs = 8
+   // 0------70------7
+   // AB\t    C\t
+   // First \t -> 6 chars width
+   // Second \t ->7 chars width
+   EXPECT_EQ(BufCharWidth('\t', 0, 3, '\0'), 3);
+   EXPECT_EQ(BufCharWidth('\t', 1, 3, '\0'), 2);
+   EXPECT_EQ(BufCharWidth('\t', 2, 3, '\0'), 1);
+   EXPECT_EQ(BufCharWidth('\t', 3, 3, '\0'), 3);
+   EXPECT_EQ(BufCharWidth('\t', 4, 3, '\0'), 2);
+   EXPECT_EQ(BufCharWidth('\t', 5, 3, '\0'), 1);
+   EXPECT_EQ(BufCharWidth('\t', 6, 3, '\0'), 3);
+
+   EXPECT_EQ(BufCharWidth(char(8), 5, 3, '\0'), 4);
+   EXPECT_EQ(BufCharWidth(char(127), 5, 3, '\0'), 5);
+
+   EXPECT_EQ(BufCharWidth('A', 5, 3, '\0'), 1);
+
+   BufSetAll(buffer, "AB_DE_GH_JK_");
+   ((char*)(BufAsString(buffer)))[2] = '\t';
+   ((char*)(BufAsString(buffer)))[5] = '\0';
+   ((char*)(BufAsString(buffer)))[7] = char(127);
+   ((char*)(BufAsString(buffer)))[11] = char(2);
+
+   char str[MAX_EXP_CHAR_LEN];
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 0, 8, str), 1);
+   EXPECT_STREQ(str, "A");
+
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 1, 8, str), 1);
+   EXPECT_STREQ(str, "B");
+
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 2, 8, str), 8);
+   EXPECT_STREQ(str, "        ");
+
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 3, 8, str), 1);
+   EXPECT_STREQ(str, "D");
+
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 5, 8, str), 5);
+   EXPECT_STREQ(str, "<nul>");
+
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 7, 8, str), 5);
+   EXPECT_STREQ(str, "<del>");
+
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 11, 8, str), 5);
+   EXPECT_STREQ(str, "<stx>");
+
+   // Only tabs
+   BufSetAll(buffer, "\t\t\t");
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 0, 8, str), 8);
+   EXPECT_STREQ(str, "        ");
+
+   memset(&str[0], '\0', sizeof(str));
+   EXPECT_EQ(BufGetExpandedChar(buffer, 1, 8, str), 8);
+   EXPECT_STREQ(str, "        ");
+}
+
+// --------------------------------------------------------------------------
+TEST_F(Ne_Text_Buffer_Test, CountDisplChars)
+{
+   BufSetAll(buffer, "AB\tC\tD\nEFG\tH");
+   buffer->tabDist = 12;
+
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 0), 0);
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 1), 1);
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 2), 2);
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 3), 12);
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 4), 13);
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 5), 24);
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 6), 25);
+
+   EXPECT_EQ(BufCountDispChars(buffer, 2, 5), 24);
+   EXPECT_EQ(BufCountDispChars(buffer, 2, 6), 25);
+
+   EXPECT_EQ(BufCountDispChars(buffer, 3, 5), 12);
+   EXPECT_EQ(BufCountDispChars(buffer, 4, 6), 13);
+
+   EXPECT_EQ(BufCountDispChars(buffer, 0, 100), 37);
+
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 100, 10), 100);
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 37, 10), 37);
+
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 25, 10), 25);
+
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 11, 10), 12);
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 10, 10), 11);
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 10, 1), 11);
+
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 8, 10), 11);
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 8, 1), 9);
+
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 0, 10), 3);
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 0, 25), 6);
+   EXPECT_EQ(BufCountForwardDispChars(buffer, 0, 100), 6);
+}
+
+// --------------------------------------------------------------------------
+TEST_F(Ne_Text_Buffer_Test, InsertCol)
+{
+   BufSetAll(buffer,
+      "AB\n"
+      "C\n"
+      "\n"
+      "D\n"
+      "EFG\n"
+      "H\n");
+
+   int charsInserted, charsDeleted;
+   BufInsertCol(buffer, 2, 5, "XYZ", &charsInserted, &charsDeleted);
+   EXPECT_STREQ(BufAsString(buffer),
+      "AB\n"
+      "C\n"
+      "  XYZ\n"
+      "D\n"
+      "EFG\n"
+      "H\n");
+   EXPECT_EQ(charsInserted, 5);
+   EXPECT_EQ(charsDeleted, 0);
+
+   BufInsertCol(buffer, 2, 16, "xyz", &charsInserted, &charsDeleted);
+   EXPECT_STREQ(BufAsString(buffer),
+      "AB\n"
+      "C\n"
+      "  XYZ\n"
+      "D\n"
+      "EFxyzG\n"
+      "H\n");
+   EXPECT_EQ(charsInserted, 6);
+   EXPECT_EQ(charsDeleted, 3);
+}
+
+// --------------------------------------------------------------------------
+TEST_F(Ne_Text_Buffer_Test, Rectangular)
+{
+   BufSetAll(buffer,
+      "AB\n"
+      "C\n"
+      "\n"
+      "D\n"
+      "EFG\n"
+      "H\n");
+
+   int charsInserted, charsDeleted;
+   BufOverlayRect(buffer, 5, 3, -1, "XY\nZ", &charsInserted, &charsDeleted);
+   EXPECT_STREQ(BufAsString(buffer),
+      "AB\n"
+      "C\n"
+      "   XY\n"
+      "D  Z\n"
+      "EFG\n"
+      "H\n");
+   EXPECT_EQ(charsInserted, 10);
+   EXPECT_EQ(charsDeleted, 2);
+
+   BufOverlayRect(buffer, 2, 2, 5, "xxx\ny\nz\nuuuuu\nv", &charsInserted, &charsDeleted);
+   EXPECT_STREQ(BufAsString(buffer),
+      "ABxxx\n"
+      "C y\n"
+      "  z\n"
+      "D uuuuu\n"
+      "EFv\n"
+      "H\n");
+   EXPECT_EQ(charsInserted, 25);
+   EXPECT_EQ(charsDeleted, 19);
+
+}
+
+// --------------------------------------------------------------------------
+TEST_F(Ne_Text_Buffer_Test, CountNLines)
+{
+   BufSetAll(buffer,
+      "AB\n"
+      "C\n"
+      "\n"
+      "D\n"
+      "EFG\n"
+      "H\n");
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 0), 0);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 1), 3);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 2), 5);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 3), 6);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 4), 8);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 5), 12);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 6), 14);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 7), 14);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 0, 8), 14);
+
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 0), 5);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 1), 6);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 2), 8);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 3), 12);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 4), 14);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 5), 14);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 6), 14);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 7), 14);
+   EXPECT_EQ(BufCountForwardNLines(buffer, 5, 8), 14);
+
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 25, 0), 14);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 15, 0), 14);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 10, 0), 8);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 10, 1), 6);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 10, 2), 5);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 10, 3), 3);
+
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 5, 0), 5);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 5, 1), 3);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 5, 2), 0);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 5, 3), 0);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 5, 4), 0);
+   EXPECT_EQ(BufCountBackwardNLines(buffer, 5, 5), 0);
+
+   EXPECT_EQ(BufCountBackwardNLines(buffer, -5, 3), 0);
+}

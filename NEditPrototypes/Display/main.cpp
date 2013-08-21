d36b138377b03abc673e1baea06269aec2101ad2
D. Fleury <dfleury2@free.fr>
2013-08-21 22:17:41 +0200

    Add Display prototype

--- NEditPrototypes/Display/main.cpp
+++ NEditPrototypes/Display/main.cpp
@@ -1,24 +1,63 @@
 #include <FL/Fl.H>
-#include <FL/Fl_Window.H>
+#include <FL/Fl_Double_Window.H>
 
 #include "../../util/fontsel.h"
 #include "../../util/Ne_FontDescription.h"
 
+#include "../../source/Ne_Text_Display.h"
+#include "../../source/Ne_Text_Buffer.h"
+
 #include <iostream>
+#include <fstream>
+#include <iterator>
 
 using namespace std;
 
+// --------------------------------------------------------------------------
 int main()
 {
-   Fl_Window win(30, 50, 600, 500, "Font Description");
-   win.show();
-
    Ne_FontDescription::InitFonts();
+   Ne_Font font;
+   for(Ne_FontDescriptions::const_iterator f = Fonts.begin(); f != Fonts.end(); ++f)
+   {
+      if (f->name.find("Consolas") != std::string::npos
+         || f->name.find("lucida") != std::string::npos)
+      {
+         font = f->getFont(12);
+         break;
+      }
+   }
 
-   Fl_Color fgPixel = fl_rgb_color(0,0,0);
-   Fl_Color bgPixel = fl_rgb_color(255,255,255);
+   ifstream file("main.cpp");
+   std::string fileStr((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+   Ne_Text_Buffer* buffer = BufCreate();
+   BufSetAll(buffer, fileStr.c_str());
 
-   FontSel(&win, ONLY_FIXED, 0, fgPixel, bgPixel);
+   Fl_Double_Window win(30, 50, 600, 500, "Display Text");
+ 
+   Ne_Text_Display* display = TextDCreate(0,0,600,500,
+      60,
+      buffer,
+      font /* NE_Font */,
+      FL_WHITE,
+      FL_BLACK,
+      FL_BLUE,
+      FL_YELLOW,
+      FL_GREEN,
+      FL_RED, 
+      FL_RED,
+      FL_BLACK,
+      0 /* continous Wrap */,
+      0 /* wrap marging */,
+      "",
+      FL_BLACK,
+      FL_YELLOW);
+
+   display->buffer = buffer;
+   //TextDResize(&display, 600,500);
+   
+   win.resizable(display);
+   win.show();
 
    return Fl::run();
 }

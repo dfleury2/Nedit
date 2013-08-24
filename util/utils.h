624fec81be4fb8cd28f1a23a3668c827cbd27315
D. Fleury <dfleury2@free.fr>
2013-08-24 22:11:02 +0200

    Use std::string for some utils

--- util/utils.h
+++ util/utils.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <vector>
-
 #include <sys/types.h>
 
 #define malloc__(size) Malloc(size, __LINE__, __FILE__)
@@ -11,11 +10,6 @@
 
 void* Malloc(int size, int line, const char* filename);
 void Free(void* arg, int line, const char* filename);
-
-#ifndef WIN32
-#include <sys/utsname.h>
-#include <sys/param.h>
-#endif
 
 #ifdef WIN32
 #define MAXPATHLEN 1024
@@ -29,17 +23,20 @@ void Free(void* arg, int line, const char* filename);
 #ifndef S_ISREG
 #define S_ISREG(mode)  (((mode) & S_IFMT) == S_IFREG)
 #endif
-#endif // WIN32
+#else
+#include <sys/utsname.h>
+#include <sys/param.h>
+#endif
 
-/* N_FILE_TYPES must be the last entry!! This saves us from counting. */
-enum {NEDIT_RC, AUTOLOAD_NM, NEDIT_HISTORY, N_FILE_TYPES};
+// N_FILE_TYPES must be the last entry!! This saves us from counting.
+enum FileTypes {NEDIT_RC, AUTOLOAD_NM, NEDIT_HISTORY, N_FILE_TYPES};
 
-/* If anyone knows where to get this from system include files (in a machine
-   independent way), please change this (L_cuserid is apparently not ANSI) */
+// If anyone knows where to get this from system include files (in a machine
+// independent way), please change this (L_cuserid is apparently not ANSI)
 #define MAXUSERNAMELEN 32
 
-/* Ditto for the maximum length for a node name.  SYS_NMLN is not available
-   on most systems, and I don't know what the portable alternative is. */
+// Ditto for the maximum length for a node name.  SYS_NMLN is not available
+// on most systems, and I don't know what the portable alternative is. */
 #ifdef SYS_NMLN
 #define MAXNODENAMELEN SYS_NMLN
 #else
@@ -48,48 +45,51 @@ enum {NEDIT_RC, AUTOLOAD_NM, NEDIT_HISTORY, N_FILE_TYPES};
 
 #define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
 
+// --------------------------------------------------------------------------
 const char* GetCurrentDir();
 const char* GetHomeDir();
 char* PrependHome(const char* filename, char* buf, size_t buflen);
 const char* NeGetUserName();
 const char* GetNameOfHost();
 int Min(int i1, int i2);
-const char* GetRCFileName(int type);
+const char* GetRCFileName(FileTypes type);
 
-/*
-**  Simple stack implementation which only keeps void pointers.
-**  The stack must already be allocated and initialised:
-**
-**  Stack* stack = (Stack*) malloc__(sizeof(Stack));
-**  stack->top = NULL;
-**  stack->size = 0;
-**
-**  NULL is not allowed to pass in, as it is used to signal an empty stack.
-**
-**  The user should only ever care about Stack, stackObject is an internal
-**  object. (So it should really go in utils.c. A forward reference was
-**  refused by my compiler for some reason though.)
-*/
-typedef struct _stackObject
+// --------------------------------------------------------------------------
+//  Simple stack implementation which only keeps void pointers.
+//  The stack must already be allocated and initialized:
+//
+//  Stack* stack = (Stack*) malloc__(sizeof(Stack));
+//  stack->top = NULL;
+//  stack->size = 0;
+//
+//  NULL is not allowed to pass in, as it is used to signal an empty stack.
+//
+//  The user should only ever care about Stack, stackObject is an internal
+//  object. (So it should really go in utils.c. A forward reference was
+//  refused by my compiler for some reason though.)
+// --------------------------------------------------------------------------
+struct stackObject
 {
    void* value;
-   struct _stackObject* next;
-} stackObject;
+   stackObject* next;
+};
 
-typedef struct
+// --------------------------------------------------------------------------
+struct Stack
 {
    unsigned size;
    stackObject* top;
-} Stack;
+};
 
 void Push(Stack* stack, const void* value);
 void* Pop(Stack* stack);
 
-
+// --------------------------------------------------------------------------
 std::string Trim(const std::string& str, const std::string& sep = " \t");
 std::vector<std::string> Split(const std::string& str, const std::string& sep);
 std::string GetStringIndex(const std::string& chaine,  int index, const char sep);
 
 std::vector<std::string::size_type> SplitPosition(const std::string& str, const char sep);
+std::string GetEnv(const char* varName);
 
 #endif /* NEDIT_UTILS_H_INCLUDED */
